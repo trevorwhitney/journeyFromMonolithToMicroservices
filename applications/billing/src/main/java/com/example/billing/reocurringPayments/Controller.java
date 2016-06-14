0317ff2a546f29caad99393e5ef3bffa82fe5bdb
Trevor Whitney <twhitney@pivotal.io>
2016-06-14 11:46:46 -0600

    Added second billing instance to start.sh
    - Proves that Eureka works across multiple instances
    - App will only pick most recently booted biling app, will not round
      robin between the two
    - A slight lag when taking down a server before app switches to the
      other, with Hysterix fallback kicking in, but does eventually fail
      over

--- applications/billing/src/main/java/com/example/billing/reocurringPayments/Controller.java
+++ applications/billing/src/main/java/com/example/billing/reocurringPayments/Controller.java
@@ -3,6 +3,7 @@ package com.example.billing.reocurringPayments;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -14,23 +15,36 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
+import static org.springframework.http.HttpStatus.*;
+import static org.springframework.web.bind.annotation.RequestMethod.*;
+
 @RestController
 public class Controller {
     @Autowired
     private com.example.payments.Gateway paymentGateway;
 
+    @Autowired
+    private Environment env;
+
     private static final Logger logger = LoggerFactory.getLogger(Controller.class);
 
-    @RequestMapping(value = "/reocurringPayment", method = RequestMethod.POST)
+    @RequestMapping(value = "/reocurringPayment", method = POST)
     public ResponseEntity<String> createReocurringPayment(@RequestBody Map<String, Object> data){
         HttpHeaders responseHeaders = new HttpHeaders();
         responseHeaders.add("content-type", MediaType.APPLICATION_JSON.toString());
 
+        logger.info("************ Creating Reocurring payment *****************");
+        logger.info("From Billing server with active profiles: ");
+        for(String profile : env.getActiveProfiles()) {
+            logger.info(profile + "\n");
+        }
+        logger.info("**********************************************************");
+
         ResponseEntity<String> response;
         if (paymentGateway.createReocurringPayment((Integer)data.get("amount"))) {
-            response = new ResponseEntity<>("{errors: []}", responseHeaders, HttpStatus.CREATED);
+            response = new ResponseEntity<>("{errors: []}", responseHeaders, CREATED);
         } else {
-            response = new ResponseEntity<>("{errors: [\"error1\", \"error2\"]}", responseHeaders, HttpStatus.BAD_REQUEST);
+            response = new ResponseEntity<>("{errors: [\"error1\", \"error2\"]}", responseHeaders, BAD_REQUEST);
         }
 
         return response;
--- start.sh
+++ start.sh
@@ -4,12 +4,12 @@ mkdir -p log
 mkdir -p tmp
 
 ./gradlew -b platform/serviceDiscovery/build.gradle clean build bootRun 2>&1 > log/eureka.log &
-eureka_pid=$1
+eureka_pid=$!
 echo $eureka_pid > tmp/eureka.pid
 echo "Starting Eurkea server with pid $eureka_pid"
 
 ./gradlew -b platform/configServer/build.gradle clean build bootRun 2>&1 > log/config.log &
-config_pid=$1
+config_pid=$!
 echo $config_pid > tmp/config.pid
 echo "Starting config server with pid $config_pid"
 
@@ -27,18 +27,29 @@ until $(curl --output /dev/null --silent --head --fail http://localhost:8888/hea
 done
 echo
 
-./gradlew -b platform/circuitBreakerDashboard/build.gradle clean build bootRun 2>&1 > log/circuitBreakerDashboard.log &
-circuit_pid=$!
-echo $circuit_pid > tmp/circuitBreakerDashboard.pid
-echo "Started circuit break dashboard with pid ${circuit_pid}"
-
 ./gradlew -b applications/billing/build.gradle clean build bootRun 2>&1 > log/billing.log &
 billing_pid=$!
 echo $billing_pid > tmp/billing.pid
 echo "Started billing with pid ${billing_pid}"
+
+./gradlew -b platform/circuitBreakerDashboard/build.gradle clean build bootRun 2>&1 > log/circuitBreakerDashboard.log &
+circuit_pid=$!
+echo $circuit_pid > tmp/circuitBreakerDashboard.pid
+echo "Started circuit break dashboard with pid ${circuit_pid}"
 
 ./gradlew -b applications/ums/build.gradle clean build bootRun 2>&1 > log/ums.log &
 ums_pid=$!
 echo $ums_pid > tmp/ums.pid
 echo "Started ums with pid ${ums_pid}"
 
+echo "Waiting for first billing server to come online"
+until $(curl --output /dev/null --silent --head --fail http://localhost:8081/health); do
+    printf '.'
+    sleep 2
+done
+echo
+
+SPRING_PROFILES_ACTIVE=secondInstance ./gradlew -b applications/billing/build.gradle bootRun 2>&1 > log/billing_2.log &
+second_billing_pid=$!
+echo $second_billing_pid > tmp/billing_2.pid
+echo "Started second billing with pid ${second_billing_pid}"
